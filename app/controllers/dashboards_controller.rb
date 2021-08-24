f65d30a0a483d0877768422faea5f838b9c56d67
Chaldrak <chaldrakus@gmail.com>
2021-08-24 23:06:29 +0100

    function to change post status ok

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,4 +2,10 @@ class DashboardsController < ApplicationController
   def index
     @articles = current_user.articles.order(created_at: :desc)
   end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @article.update(status: !@article.status)
+    redirect_to dashboard_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'dashboards/index', to: 'dashboards#index', as: 'dashboard'
+  resources :dashboards, only: [:index, :create]
 
   mount LetterOpenerWeb::Engine, at: "/inbox" if Rails.env.development?
   devise_for :users
